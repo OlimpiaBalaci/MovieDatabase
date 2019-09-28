99f76b4b5c78ba37daa968f39ae87ac69650b779
Olimpia Balaci <olimpia.balaci@yahoo.com>
2019-09-28 13:59:37 +0300

    add new class

--- src/com/balacio/Main.java
+++ src/com/balacio/Main.java
@@ -1,17 +1,24 @@
 package com.balacio;
 
+import com.balacio.constants.Constants;
+import com.balacio.db.DatabaseWrapper;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        //old
+        //region read data from terminal
+    /*    // write your code here
         //System.out.println("test");
-        Connection connection; //used to open
+        Connection connection = null; //used to open
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306",
-                    "root", "maxim11forever");
+            connection = DriverManager.getConnection(Constants.DB_DRIVER_IMPLEMENTATION + "://localhost:3306/imdb",
+                    Constants.DB_USERNAME, Constants.DB_PASSWORD);
             System.out.println("Connection Succesfully!");
 
 
@@ -19,5 +26,24 @@ public class Main {
             System.out.println(ex.getMessage());
             System.out.println(ex.getStackTrace());
         }
+        //endregion*/
+        DatabaseWrapper databasewrapper = new DatabaseWrapper();
+        Connection connection = null;
+        try {
+            connection = databasewrapper.createConnection(
+                    Constants.DB_DRIVER_IMPLEMENTATION +"://"+ Constants.DB_CONN_STRING,
+                            Constants.DB_USERNAME, Constants.DB_PASSWORD
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+                //statement created from JDBC connection
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE movies(id INTEGER PRIMARY KEY AUTO_INCREMENT, name TEXT NOT NULL)");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
 }
